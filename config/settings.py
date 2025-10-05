9c758a3e04450660ba2b60330d2fb870b27b823c
tnka <tnka@xyz.com>
2025-10-05 19:10:26 -0300

    feat: update jwt service

--- config/settings.py
+++ config/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("DJANGO_SECRET")
+JWT_SECRET = config("JWT_SECRET")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
